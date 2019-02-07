695ca5d4d993a02aed4101543cac18ced7f222f3
jgmaspons <georgemaspons@gmail.com>
2019-02-07 04:45:42 -0500

    part 1-partially completed

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,8 +9,12 @@ input_file = os.path.join('../Resources/', 'budget_data.csv')
 # Lists to store data
 dateRec = []
 profit_loss = []
+value_sum = []
 change_profit_loss = []
-#avg_change_profit_loss = []
+
+# Define initial values of store data lists
+value_sum = 0
+change_profit_loss = 0
 
 # Open and read csv
 with open(input_file, 'r', newline="") as budget_data:
@@ -18,49 +22,52 @@ with open(input_file, 'r', newline="") as budget_data:
 
 # Read the header row first 
     csv_header = next(csvreader)
-
-                    # Define the function and have it accept the 'budgetData' as its sole parameter
-                    #def getAnalysis(budgetData):
-    
-    #Total number of months included in the dataset
+             
+    # Iterate row-by-row through the dataset    
     for row in csvreader:
 
+        # Add date data to its respective list
         dateRec.append(row[0])
+
+        # Add profit/loss data to its respective list
         profit_loss.append(row[1])
+            
+        # Calculate total number of months included in the dataset
+        value = len(list(profit_loss))
         
-        value = len(list(budget_data))
-        print(value)
+        
+        # Calculate net total amount of profit/losses over the entire period
+        value_sum += float(row[1])
+         
+    # Add change in profit/loss data to its respective list
+    #for i in len(list(profit_loss): 
+        #change_profit_loss.append(float(profit_loss([i]) - float(profit_loss([i-1]))     
 
-    # Net total amount of Profit/Losses over the entire period
-    #value_sum = 0
-    #for row in csvreader:  
 
-        #value_sum += float(row[1])
+        # Add data to its range list
+        
+        #def average(change_profit_loss):
+            #length = len(list(change_profit_loss)
+                  
+            #for average in change_profit_loss
+                #change_profit_loss += float(row[
+            #return length
 
-        
-        #print(value_sum)               
-                    
-                    
-    # The average of the changes in "Profit/Losses" over the entire period 
-        #for row in csvreader:
-            #dateRec.append(row[0])
-            #profit_loss.append(row[1])
-    
-    
-    for i in range(1, len(profit_loss)):
-        
-        change_profit_loss.append(float(profit_loss[i]) - float(profit_loss[i-1]))
- 
-        
-        #print (max(change_profit_loss))
-        #print(min(change_profit_loss))
 
         #avg_change_profit_loss = 
         #print(change_profit_loss)
-        average_change = (sum(change_profit_loss)) / value
-        print(average_change)
+        #average_change = sum(change_profit_loss) / value
+        #print(average_change)
             
-      
+    # Print results related total months count
+    print(value)
+
+    # Print results related to net total amount of profit/losses
+    print(value_sum)
+
+    # Print results related to avg. change in profit/loss amount
+    #print(average(change_profit_loss))
+    #print(change_profit_loss)
 
     # The greatest increase in profits (date and amount) over the entire period
 
@@ -68,38 +75,7 @@ with open(input_file, 'r', newline="") as budget_data:
     # The greatest decrease in losses (date and amount) over the entire period
 
 
+        #print (max(change_profit_loss))
+        #print (min(change_profit_loss))
 
-                    # Read in the CSV file
-
-                    #with open(input_file, 'r', newline="") as budget_data:
-
-                        # Split the data on commas
-                        #csvreader = csv.reader(budget_data, delimiter=',')
-                        #csv_header = next(csvreader)
-
-                        # Split the data on commas
-                        
-                        
-                        # Write data to a .csv file
-                        # with open(pybankCSV, "w", newline="") as csvfile:
-                            # writer = csv.writer(csvfile)
-
-                        # To save specific data input as a row in the csv file
-                            # writer.writerow(["row1", "row2"])
-
-
-                        #header = next(csvreader)
-
-                        # Loop through the data
-                    
-
-                        
-                        # Run the 'financialAnalysis()' function 
-                        #getAnalysis(row)
-
-                        #for row in csvreader:
-
-                            #if float(row[1]) > 0:
-                                #print(row)
-
-   
+             
