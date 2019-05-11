86754d00e1d95eb5290a2d52cacff56c95ebacf5
jgmaspons <georgemaspons@gmail.com>
2019-05-11 17:14:32 -0400

    D# final draft

--- D3/Homework/StarterCode/assets/js/app.js
+++ D3/Homework/StarterCode/assets/js/app.js
@@ -34,24 +34,13 @@ var chartGroup = svg.append("g")
 //= ================================  
 
 // Import the data
-d3.csv("data.csv").then(data =>{
-      
-
-// Print the data    
-    //console.log(data);
-
-// Parse the data and format the data as numbers
-data.forEach(function(data) {
-    data.poverty = +data.poverty;
-    data.healthcare = +data.healthcare;  
- 
-});  
-    console.log(data);
+d3.csv("data.csv").then(function(healthData){
 
 // Scaling the graph into proportions
-
 var yLinearScale = d3.scaleLinear().range([height, 0]);
 var xLinearScale = d3.scaleLinear().range([0, width]);
+
+console.log(healthData);
 
 // Setting variables to store max & min values
 var xMin;
@@ -59,21 +48,29 @@ var xMax;
 var yMin;
 var yMax;
 
+// console.log(yMin);
+// console.log(yMax);
+// console.log(xMin);
+// console.log(xMax);
+
+// Parse the data and format the data as numbers
+healthData.forEach(function(data) {
+    data.poverty = +data.poverty;
+    data.healthcare = +data.healthcare; 
+
 // Finding min and max for y-axis and x-axis
 
-yMin = d3.min(data, d => d.healthcare);
-yMax = d3.max(data, d => d.healthcare);
+yMin = d3.min(healthData, d => d.healthcare);
+yMax = d3.max(healthData, d => d.healthcare);
 
-xMin = d3.min(data, d => d.poverty);
-xMax = d3.max(data, d => d.poverty);
+xMin = d3.min(healthData, d => d.poverty);
+xMax = d3.max(healthData, d => d.poverty);
 
-yLinearScale.domain([yMin, yMax*1.1]);
-xLinearScale.domain([xMin, xMax*1.05]);
+yLinearScale.domain([yMin * 0.85, yMax * 1.02]);
+xLinearScale.domain([xMin * 0.98 , xMax * 1.04]);
 
-console.log(yMin);
-console.log(yMax);
-console.log(xMin);
-console.log(xMax);
+
+});// END FOR EACH LOOP
 
 // Step 3: Create the axies and display them
 //= ================================  
@@ -90,5 +87,53 @@ chartGroup.append("g")
     .attr("transform", `translate(0, ${height})`)
     .call(bottomAxis);
 
-});
 
+// Step 4: Load data into graph
+//= ================================  
+
+// Create circles
+var circlesGroup = chartGroup.selectAll("circle")
+  .data(healthData)
+  .enter()
+  .append("circle")
+  .attr("cx", d => xLinearScale(d.poverty))
+  .attr("cy", d => yLinearScale(d.healthcare))
+  .attr("r", "10")
+  .attr("fill", "#0066cc")
+  .attr("opacity", .5)
+
+// Add labels to circles
+
+chartGroup.append("text")
+.style("font-size", "10px")
+.selectAll("tspan")
+.data(healthData)
+.enter()
+.append("tspan")
+    .attr("x", function(data) {
+        return xLinearScale(data.poverty -0.1);
+    })
+    .attr("y", function(data) {
+        return yLinearScale(data.healthcare -0.1);
+    })
+    .text(function(data) {
+        return data.abbr
+    });
+
+ // Add axis labels   
+
+chartGroup.append("text")
+  .attr("transform", "rotate(-90)")
+  .attr("y", 0 - margin.left)
+  .attr("x", 0 - (height / 1.5))
+  .attr("dy", "1em")
+  .attr("class", "axisText")
+  .text("Lacks Healthcare (%)");
+
+chartGroup.append("text")
+  .attr("transform", `translate(${width / 2.25}, ${height = margin.top + 450})`)
+  .attr("font-size", "16px")
+  .attr("class", "axisText")
+  .text("In Poverty (%)");
+
+});
